be6b0408b06ed5e84cf66a0cfe33b6cfac97315a
Gatlat Both <gatlatboth@gmail.com>
2025-08-03 12:45:49 +0300

    all the new changes

---
+++ jobs/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Job(models.Model):
+    JOB_TYPES = (
+        ('full-time', 'Full-Time'),
+        ('part-time', 'Part-Time'),
+        ('contract', 'Contract'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    company = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=20, choices=JOB_TYPES)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ jobs/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets, filters
+from .models import Job
+from .serializers import JobSerializer
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all().order_by('-posted_at')
+    serializer_class = JobSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['location', 'category', 'job_type']
